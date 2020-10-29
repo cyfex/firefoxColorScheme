7d425354e6eb2ae78730c85e7bb86b43f67f0eb6
youfu <cyfv17@gmail.com>
2020-10-29 19:28:42 +0800

    Fixed windows issues and add a color scheme for windows

---
+++ pseudo-windows/user.js
@@ -0,0 +1,5 @@
+user_pref("browser.anchor_color", "#4b8910");
+user_pref("browser.display.background_color", "#202020");
+user_pref("browser.display.foreground_color", "#aea79f");
+user_pref("browser.visited_color", "#3465a4");
+user_pref("ui.textSelectBackgroundAttention", "#E46D43");
